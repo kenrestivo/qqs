6d8ec5595a419d33235c5d5a1066558912e53ec3
ken restivo <krestivo@restivo.org>
2012-06-07 22:55:43 -0700

    bring in compile-time requirements

--- project.clj
+++ project.clj
@@ -3,4 +3,15 @@
   :url "http://github.com/kenrestivo/qqs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.cemerick/friend "0.0.8"]
+                 [com.google.step2/step2-consumer "1-SNAPSHOT"]
+                 ;; to force around a bug in httpclient 4.1.x [0]
+                 [org.apache.httpcomponents/httpclient "[4.2-beta1]"]]
+  ;; TODO: put step2 on clojars or somewhere more permanent
+  :repositories {"kens" "http://www.restivo.org/mvn/"})
+
+
+
+;;; [0] http://forum.springsource.org/showthread.php?114685-Samples-OpenId-Google-Login-Hostname-in-Certificate-didn-t-match
+
--- test/qqs/core_test.clj
+++ test/qqs/core_test.clj
@@ -3,13 +3,15 @@
         qqs.core))
 
 
-;; not really a test, more like a usage example
-;; I needed to authenticate people based on email, where their UID is email
-;; and they may have > 1 email address in their Google Apps account
 
 (def step2-users {"foo@bar.com"
                {:username "foo"
                 :roles #{::user ::admin}}})
+
+
+;; not really a test, more like a usage example for a credential function
+;; I needed to authenticate people based on email, where their UID is email
+;; and they may have > 1 email address in their Google Apps account
 
 
 (defn step2-creds
